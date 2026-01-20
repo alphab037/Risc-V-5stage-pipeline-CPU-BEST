b92fd8b58750789f8a39c3d0222c13bee509a89f
JiminByeon <shjm0108@gmail.com>
2026-01-21 02:01:46 +0900

    Update datapath.v

--- cpu1.srcs/sources_1/new/datapath.v
+++ cpu1.srcs/sources_1/new/datapath.v
@@ -27,6 +27,7 @@ wire [1:0]  jW;
 
 
 // HAZARD UNIT 
+    
 data_forwarding T1(.rs1_exe(rs1E),.rs2_exe(rs2E),.rd_mem(rdM),.rd_wb(rdW),.afwd(afwd),.bfwd(bfwd));
 interlock T2(.rs1_id(instrD[19:15]), .rs2_id(instrD[24:20]), .rd_exe(rdE),.MemRead_exe(memtoregE), .stall(stall));
 
@@ -36,6 +37,7 @@ interlock T2(.rs1_id(instrD[19:15]), .rs2_id(instrD[24:20]), .rd_exe(rdE),.MemRe
     rv32i_cpu u1(.CLK(clk),.reset(reset),.stall(stall),.a(jE[1]),.b(jE[0]),.c(btaken),.in(Mux3),.PC(PC));
 memoryI u2(.Addr(PC), .rData(instruction));
 mux3to1b m1(.a(btaken),.b(jE[1]),.c(jE[0]),.x1(instruction),.r(instr));
+    
 IF_ID A1(.clk(clk),.reset(reset),.clear(),.enable(~stall),.PCF(PC),.instrF(instr),.PCD(PCD),.instrD(instrD));
 
 //Instruction DECODE
@@ -43,7 +45,6 @@ IF_ID A1(.clk(clk),.reset(reset),.clear(),.enable(~stall),.PCF(PC),.instrF(instr
 regfile u3(.clk(clk),.we(regwriteW), .rs1(instrD[19:15]), .rs2(instrD[24:20]), .rd(rdW), .rd_data(Mux2), .rs1_data(rs1_data), .rs2_data(rs2_data), .x1(x1), .x2(x2), .x3(x3));
 signex u4(.instr(instrD),.PC(PCD),.imm_ext(imm_ext));
 maindec u5(.opcode(instrD[6:0]),.funct3(instrD[14:12]),.stall(stall),.btaken(btaken),.jE1(jE[1]),.jE2(jE[0]), .RegWrite(RegWrite),.memWrite(memWrite),.memtoReg(memtoReg),.ALUsrc(ALUsrc),.beq(beq),.bne(bne),.blt(blt),.bge(bge),.bltu(bltu),.bgeu(bgeu),.jal(jal),.jalr(jalr));
-
 aludec u6(.opcode(instrD[6:0]),.funct7(instrD[31:25]),.funct3(instrD[14:12]),.ALUcontrol(ALUcontrol));
 
 ID_EX A2(.clk(clk),.reset(reset),.enable(1),.clear(),.PCD(PCD),.rs1(instrD[19:15]),.rs2(instrD[24:20]), .rdD(instrD[11:7]),.rs1_dataD(rs1_data),.rs2_dataD(rs2_data), .imm_extD(imm_ext),
@@ -52,13 +53,13 @@ C_ID_EX B1(.clk(clk),.reset(reset),.enable(1),.clear(),.regwriteD(RegWrite), .me
            .regwriteE(regwriteE),.memwriteE(memwriteE),.memtoregE(memtoregE),.ALUsrcE(alusrcE),.ALUcontE(alucontE),.branchE(branchE),.jE(jE),.funct3E(funct3E));
 
 //Execution 
-
-mux3to1a m2(.a(afwd),.x1(ALU_outputM),.x2(Mux2),.x3(rs1_dataE),.r(result));
+    
 alu u7(.a(result),.b(Mux1),.alucont(alucontE),.result(ALU_output),.N(N),.Z(Z),.C(C),.V(V));
-mux4to1 m3(.a(alusrcE),.b(bfwd),.x1(imm_extE),.x2(rs2_dataE),.x3(ALU_outputM),.x4(Mux2),.r(Mux1));
 branchdec u8(.beq(branchE[5]),.bne(branchE[4]),.blt(branchE[3]),.bge(branchE[2]),.bltu(branchE[1]),.bgeu(branchE[0]),.N(N),.C(C),.Z(Z),.V(V),.btaken(btaken));
-
+mux3to1a m2(.a(afwd),.x1(ALU_outputM),.x2(Mux2),.x3(rs1_dataE),.r(result));
+mux4to1 m3(.a(alusrcE),.b(bfwd),.x1(imm_extE),.x2(rs2_dataE),.x3(ALU_outputM),.x4(Mux2),.r(Mux1));
 mux3to1 m4(.a(btaken),.b(jE[1]),.c(jE[0]),.x1(imm_extE+PCE),.x2(imm_extE+rs1_dataE),.x3(PCE),.r(Mux3));
+
 EX_MEM A3(.clk(clk),.reset(reset),.enable(1),.clear(),.PCE(PCE),.rdE(rdE),.ALU_outputE(ALU_output),.rs2_dataE(rs2_dataE),
           .PCM(PCM),.rdM(rdM),.ALU_outputM(ALU_outputM),.rs2_dataM(rs2_dataM));
 C_EX_MEM B2(.clk(clk),.reset(reset),.enable(1),.clear(),.regwriteE(regwriteE), .memwriteE(memwriteE),.memtoregE(memtoregE),.jE(jE),.funct3E(funct3E),
@@ -67,9 +68,9 @@ C_EX_MEM B2(.clk(clk),.reset(reset),.enable(1),.clear(),.regwriteE(regwriteE), .
 //Memory Access
 
 memoryd u9(.Addr(ALU_outputM),.memwrite(memwriteM),.funct3(func3M),.clk(clk),.wData(rs2_dataM),.rData(rData));
-
 MEM_WB A4(.clk(clk),.reset(reset),.enable(1),.clear(),.PCM(PCM),.ALU_outputM(ALU_outputM),.rDataM(rData),.rdM(rdM),
          .PCW(PCW), .ALU_outputW(ALU_outputW),.rDataW(rDataW),.rdW(rdW));
+
 C_MEM_WB B4(.clk(clk),.reset(reset),.enable(1),.clear(),.memwtoregM(memwtoregM),.regwriteM(regwriteM),.jM(jM),
            .memwtoregW(memwtoregW),.regwriteW(regwriteW),.jW(jW));
 
@@ -79,4 +80,5 @@ mux3to1 m5(.a(jW[1]),.b(jW[0]),.c(memtoregW),.x1(PCW+4),.x2(rDataW),.x3(ALU_outp
 
 
 endmodule
+
 
