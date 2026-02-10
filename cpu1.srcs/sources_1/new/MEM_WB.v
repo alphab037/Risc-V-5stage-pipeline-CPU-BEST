cfe17779a251b85db6df29e2f81564d5d0b8274c
JiminByeon <shjm0108@gmail.com>
2026-02-10 19:37:47 +0900

    Update MEM_WB.v

--- cpu1.srcs/sources_1/new/MEM_WB.v
+++ cpu1.srcs/sources_1/new/MEM_WB.v
@@ -1,30 +1,32 @@
 `timescale 1ns / 1ps
 
-module MEM_WB(
-    input clk, reset, enable, clear,
-    input [31:0] PCM, ALU_outputM, rDataM, rdM,
-    output reg [31:0] PCW,ALU_outputW, rDataW, rdW
+module EX_MEM(
+    input clk,reset,enable,clear,
+    input [31:0] PCE, ALU_outputE, rs2_dataE,
+    input [4:0] rdE,
+    output reg [4:0] rdM,
+    output reg [31:0] PCM,ALU_outputM, rs2_dataM
     );
 
-always @(posedge clk, posedge reset) begin
-    if (reset) begin
-        PCW <= 0;
-        ALU_outputW <= 0;
-        rDataW <= 0;
-        rdW <= 0;
+always @( posedge clk, posedge reset ) begin
+    if (reset) begin 
+        PCM <= 0;
+        rdM <= 0;
+        ALU_outputM <= 0;
+        rs2_dataM <= 0;
     end
-    else if (enable) begin
-        if (clear) begin
-            PCW <= 0;
-            ALU_outputW <= 0;
-            rDataW <= 0;
-            rdW <= 0;
+    else if (enable) begin 
+        if (clear) begin 
+            PCM <= 0;
+            rdM <= 0;
+            ALU_outputM <= 0;
+            rs2_dataM <= 0;
         end
-        else begin
-            PCW <= PCM;
-            ALU_outputW <= ALU_outputM;
-            rDataW <= rDataM;
-            rdW <= rdM;
+        else begin	 
+            PCM <= PCE;
+            rdM <= rdE;
+            ALU_outputM <= ALU_outputE;
+            rs2_dataM <= rs2_dataE;
         end
     end
 end
